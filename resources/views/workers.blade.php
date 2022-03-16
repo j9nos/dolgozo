cfddd1f9987306d54789a436c15fa19fc6d9f414
j9nos <k75681146744@ktch.hu>
2022-03-16 09:08:06 +0100

    in table

--- resources/views/workers.blade.php
+++ resources/views/workers.blade.php
@@ -1,9 +1,18 @@
 <div>
-    <ul>
-    @foreach($workers as $worker)
-    <li>
-        {{$worker->name}}
-    </li>
+    <table>
+  <tr>
+    <th>Név</th>
+    <th>Város</th>
+    <th>Születés</th>
+    <th>Fizetés</th>
+  </tr>
+  @foreach($workers as $worker)
+    <tr>
+    <td>{{$worker->name}}</td>
+    <td>{{$worker->city}}</td>
+    <td>{{$worker->birthdate}}</td>
+    <td>{{$worker->salary}}</td>
+    </tr>
     @endforeach
-    </ul>
+</table>
 </div>
